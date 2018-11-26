ac71efb5afb48047be6e7bd1cbf5b07786385052
watakemi725 <takemi007@hotmail.com>
2018-11-26 15:43:49 +0900

    Initial Commit

---
+++ Nodehands_client_wwg/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nodehands_client_wwg
+//
+//  Created by watakemi725 on 2018/11/26.
+//  Copyright © 2018年 watanuki takemi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
